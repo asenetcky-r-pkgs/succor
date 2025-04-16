c25c712a7e0e6c5ee05ec040ce19eca8589f9718
asenetcky <asenetcky@gmail.com>
2025-04-15 20:02:49 -0400

    feat: add package

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,12 @@
 # succor 0.2.0
 
+## Features
+
+- Added environment helpers
+
 # succor 0.1.0
+
+## Features
+
+- Added data helpers
 
---
+++ R/succor-package.R
@@ -0,0 +1,8 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom tibble tibble
+#' @importFrom lubridate make_date
+## usethis namespace: end
+NULL
